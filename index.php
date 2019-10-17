970bf8fd52770d927db4c73bc782f89f92b21e37
LewisSama <lou.lesage09@gmail.com>
2019-10-17 16:13:32 +0200

    new class with new parent

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <?php
 require_once 'Bicycle.php';
+require_once 'Car.php';
+require_once 'Truck.php';
 $bike = new Bicycle('rouge', 1);
-$mustang = new Car('noir', 5,'essence');
+$mustang = new Car('noir', 5,'fuel');
 $veloVeloce = new Bicycle('bleu', 1);
-$voitureVeloce = new Car('jaune', 5, 'electrique');
+$voitureVeloce = new Car('jaune', 5, 'electric');
+$grosCamion = new Truck('blue', 2, 'fuel', 80);
+$grosCamionRempli = new Truck('red', 2, 'fuel', 100);
 
-echo $mustang->start();
+var_dump($grosCamionRempli);
+$grosCamionRempli->setStock(100);
+var_dump($grosCamionRempli);
+echo $grosCamionRempli->isTruckFull();
 
-var_dump($bike);
-var_dump($mustang);
-
-$bike->forward();
-$mustang->forward();
-
-var_dump($bike);
-var_dump($mustang);
-
-$bike->brake();
-$mustang->brake();
-
-var_dump($bike);
-var_dump($mustang);
+$grosCamionRempli->forward();
+var_dump($grosCamionRempli);
+$grosCamionRempli->brake();
+var_dump($grosCamionRempli);
